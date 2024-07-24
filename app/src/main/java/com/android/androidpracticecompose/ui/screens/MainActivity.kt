1e9f3e671e9b56f47b1f35753a3e8a1ca6f4149c
Malik Saif <25033101+maliksaif@users.noreply.github.com>
2024-07-24 10:25:20 +0500

    architecture setup

--- app/src/main/java/com/android/androidpracticecompose/ui/screens/MainActivity.kt
+++ app/src/main/java/com/android/androidpracticecompose/ui/screens/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.android.androidpracticecompose
+package com.android.androidpracticecompose.ui.screens
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.androidpracticecompose.ui.theme.AndroidPracticeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,22 +28,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidPracticeComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting(name = "Android")
                 }
             }
         }
     }
+
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Blue,
+        )
+        Text(
+            text = "Second Text",
+            color = Color.Blue,
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
